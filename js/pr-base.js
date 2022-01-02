03876a62167bf27f74d14a5fd21fe66a74412b5d
Ahmed Dardery <Megadardery@yahoo.com>
2022-01-02 17:17:08 +0200

    fix pr-base for force pushes

--- js/pr-base.js
+++ js/pr-base.js
@@ -1,15 +1,22 @@
 $(document).bind('ext:load', function () {
   $(".commit-ref").each(function () {
-    branch_block = $(this).find("a:first")
+    branch_block = $(this).find(":first-child")
 
     branch_name = branch_block.find("span:last").text();
 
-    branch_url = branch_block.attr('href');
-    repo_url = branch_url.match(/(?:\/[^\/]+){2}/)[0]
+    function getRepoUrl(branch_block){
+      branch_url = branch_block.attr('href');
+      if(!branch_url)
+        branch_url = window.location.pathname;
+      matches = branch_url.match(/(?:\/[^\/]+){2}/);
+      return matches[0];
+    }
 
-    params = $.param({'q':`is:pr is:open base:${branch_name}`})
-    href = `${repo_url}/pulls?${params}`
+    repo_url = getRepoUrl(branch_block);
 
-    $(this).after(` (<a href="${href}">PRs</a>)`)
+    params = $.param({ 'q': `is:pr is:open base:${branch_name}` });
+    href = `${repo_url}/pulls?${params}`;
+
+    $(this).after(` (<a href="${href}">PRs</a>)`);
   });
 });
